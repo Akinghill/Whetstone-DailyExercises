9df9f74909ce3a003e531e3cf721e66027af13a8
Akinghill <Austinkinghill@gmail.com>
2020-04-27 13:50:35 -0700

    A couple days of exercises

---
+++ DailyExercise04-27.js
@@ -0,0 +1,46 @@
+/*
+
+Good day Developer. I would like to try out doing daily exercises.
+Some of this might seem like tedious review, and some might seem like 
+impossible algorithms. Both are great and give you a chance to learn:D
+
+Today's exercise should be a review, but it might seem tricky since we
+are returning from a long break. 
+
+Copy this file and paste it on your desktop to use it. 
+When you have the problems complete, upload your solution with
+the following format to the shared drive in the "Daily Exercise > am/pm-submit" folder
+
+file naming format should mimic this:
+
+Hill-DailyExercise04-27.js
+
+*/
+
+
+/*
+	1. Write a function that takes an array as a parameter and returns
+	the highest number from that array. 
+
+*/ 
+
+
+
+
+
+
+
+//test cases below for #1
+highestNum([1,2,3,4])         //should return 4
+highestNum([-40,20,-100,21])  //should return 21
+
+/*
+	2. Write a function that takes an array as a parameter and returns
+	the highest odd number. 
+*/ 
+
+
+
+//test cases below for #2
+highestOddNum([1,2,3,4])         //should return 3
+highestOddNum([-40,20,-100,21])  //should return 21
---
+++ DailyExercise04-29.js
@@ -0,0 +1,27 @@
+/*
+
+Good day Developer, 
+
+For today's daily exercise I would like to see that everyone is able
+to create and understand what a Markdown file is.
+
+Follow the users stories below and submit your Markdown file to the appropriate folder.
+
+File naming format should mimic this:
+Don't use the curly braces. These are just to show that this values will change.
+{Hill}-DailyExercise{04-27}.{js}
+
+*/
+
+
+/*
+User Stories
+
+1. Create a Markdown file in a text editor.
+2. Create an h1 and h2 in the file. 
+3. Create an ordered list, that contains 3 things that would help you learn remotely.
+4. Create a link that says "I am a link" and links to google's homepage.
+5. Create a fenced in code block and write a forloop inside that block.
+6. In regular text, write a couple sentences explaining why developers create README.md files.
+
+*/ 

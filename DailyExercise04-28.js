b677d9b8f367c98c495de99b3fe20ac937cd0f64
Akinghill <Austinkinghill@gmail.com>
2020-04-27 14:58:37 -0700

    Daily Exercise 28
    Review .map, .filter, and js objects

---
+++ DailyExercise04-28.js
@@ -0,0 +1,45 @@
+/*
+
+Good day Developer, 
+
+Let's review some slightly more advanced JavaScript. 
+
+*/
+
+
+/*
+	1. Create a function that takes two parameters, an array of numbers. Using the map method,
+	return a new array that contains every value from the input array with 10 added to each.
+	The input array should not change!
+*/ 
+
+
+
+// 1. Test Cases
+addTen([1,2,3,4,5]) // returns [11,12,13,14,15]
+addTen([45,13,0,-20,1]) // returns [55,23,10,-10,11]
+
+
+/*
+	2. Create a function that takes two parameters, an array of numbers. Using the filter method,
+	return a new array that contains every even number from the input array.
+	The input array should not change!
+*/ 
+
+
+
+
+
+// 2. Test Cases
+getEvens([1,2,3,4,5]) // returns [11,12,13,14,15]
+getEvens([45,13,0,-20,1]) // returns [55,23,10,-10,11]
+
+
+/*
+	3. Create a JavaScript object named car. Give it the following key value pairs.
+	The car should have:
+	a name that is a string,
+	4 wheels,
+	a boolean called isFast,
+	a method called "drive"
+*/ 

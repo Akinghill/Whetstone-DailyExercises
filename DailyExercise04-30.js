4064352c9136cdd6418e651fb49e3d3c8b0d3e35
Austin Hill <austinkinghill@gmail.com>
2020-05-01 06:51:07 -0700

    05-01 Exercise

--- DailyExercise04-30.js
+++ DailyExercise04-30.js
@@ -11,28 +11,28 @@ Let's review some slightly more advanced JavaScript.
 	1. Create a function that takes two parameters, an array of numbers. Using the map method,
 	return a new array that contains every value from the input array with 10 added to each.
 	The input array should not change!
-*/ 
+*/
 
 
 
 // 1. Test Cases
-addTen([1,2,3,4,5]) // returns [11,12,13,14,15]
-addTen([45,13,0,-20,1]) // returns [55,23,10,-10,11]
+addTen([1, 2, 3, 4, 5]) // returns [11,12,13,14,15]
+addTen([45, 13, 0, -20, 1]) // returns [55,23,10,-10,11]
 
 
 /*
 	2. Create a function that takes two parameters, an array of numbers. Using the filter method,
 	return a new array that contains every even number from the input array.
 	The input array should not change!
-*/ 
+*/
 
 
 
 
 
 // 2. Test Cases
-getEvens([1,2,3,4,5]) // returns [11,12,13,14,15]
-getEvens([45,13,0,-20,1]) // returns [55,23,10,-10,11]
+getEvens([1, 2, 3, 4, 5]) // returns [11,12,13,14,15]
+getEvens([45, 13, 0, -20, 1]) // returns [55,23,10,-10,11]
 
 
 /*
@@ -42,4 +42,4 @@ getEvens([45,13,0,-20,1]) // returns [55,23,10,-10,11]
 	4 wheels,
 	a boolean called isFast,
 	a method called "drive"
-*/ 
+*/

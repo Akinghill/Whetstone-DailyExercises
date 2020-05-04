0cd3cbbd944a6c88bed98616832d1aadfe2a523c
Austin Hill <austinkinghill@gmail.com>
2020-05-03 22:04:13 -0700

    droid not driod

--- DailyExercise05-04.js
+++ DailyExercise05-04.js
@@ -1,8 +1,8 @@
 /* 
   Good day developer. In honor of May the 4th, let's take a look at a franchise that should be known by all developers, Star Wars. 
-  In Star Wars, driods are given a name of numbers and letters like R2-D2, C3-PO, and BB-8.
+  In Star Wars, droids are given a name of numbers and letters like R2-D2, C3-PO, and BB-8.
 
-  Write a function that generates a random name for a driod. The droid names should follow the following rules:
+  Write a function that generates a random name for a droid. The droid names should follow the following rules:
     * Names starts with a letter
     * Names are five characters long with a dash in the middle (example: XX-XX)
     * Names contain at least 1 number
@@ -15,3 +15,8 @@
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var letters = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
 
+function generateName() {
+
+}
+
+console.log(generateName())
